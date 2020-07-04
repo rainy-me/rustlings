5241a817f46c0c3d7ae2963bb0795a01cac74ef3
rainy-me <github@rainy.me>
2020-07-04 23:04:20 +0900

    update

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,13 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
+// I AM DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move { x: u8, y: u8 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -36,7 +39,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            // is there a way to pass enum data without destructuring?
+            Message::ChangeColor(a,b,c) => self.change_color((a,b,c)),
+            Message::Echo(val) => self.echo(val),
+            Message::Move {x,y} => self.move_position(Point {x,y}),
+            Message::Quit => self.quit(),
+        };
     }
 }
 
